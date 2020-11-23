04b445f6725d01d901b8c9b0fe0f4118d9a3644a
23arif <arifaov@outlook.com>
2020-11-23 22:25:00 +0400

    Added slider.Some other changes.

--- js/script.js
+++ js/script.js
@@ -12,4 +12,23 @@ function changeTab(type,t) {
     }
 
 }
+$(document).ready(function(){
 
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+});
